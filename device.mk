13a4615d0d7c0ea46b7d6600187aace722263360
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-08-02 23:28:38 -0500

    santoni:omg I derped this
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -289,6 +289,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
-PRODUCT_COPY_FILES += \
-   $(LOCAL_PATH)/prebuilt/wlan.ko:system/lib/modules/wlan.ko
-
